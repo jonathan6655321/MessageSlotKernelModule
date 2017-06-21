13c1274a6dbc65331ab8295931a1ff70654d51ec
Jonathan Somer <somer@mail.tau.ac.il>
2017-06-22 02:54:12 +0300

    real ugly coding..

--- message_sender.c
+++ message_sender.c
@@ -5,4 +5,51 @@
  *      Author: Jonathan
  */
 
+#include "message_slot.h"
 
+#include <fcntl.h>      /* open */
+#include <unistd.h>     /* exit */
+#include <sys/ioctl.h>  /* ioctl */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+
+	int channelIndex = argv[1];
+	if (channelIndex > NUM_CHANNELS - 1 || channelIndex < 0) {
+		printf("Error: wrong channel index argument");
+		return -1;
+	}
+
+	char message[MESSAGE_BUFFER_LENGTH] = argv[2];
+
+
+
+	int file_desc, ret_val;
+
+	file_desc = open("/dev/"DEVICE_FILE_NAME, 0);
+	if (file_desc < 0) {
+		printf("Can't open device file: %s\n",
+		DEVICE_FILE_NAME);
+		exit(-1);
+	}
+
+	ret_val = ioctl(file_desc, IOCTL_SET_ENC, *channelIndex);
+
+	if (ret_val < 0) {
+		printf("ioctl_set_msg failed:%d\n", ret_val);
+		exit(-1);
+	}
+
+	ret_val = write(file_desc, message, MESSAGE_BUFFER_LENGTH);
+	if (ret_val < 0) {
+			printf("write failed:%d\n", ret_val);
+			exit(-1);
+		}
+
+	close(file_desc);
+
+	printf("Wrote message %s to channel number: %d", message, channelIndex);
+	return 0;
+
+}
--- message_slot.h
+++ message_slot.h
@@ -9,5 +9,32 @@
 #define MESSAGE_SLOT_H_
 
 
+// FROM CHARDEV.H:
+
+
+#include <linux/ioctl.h>
+
+
+
+/* The major device number. We can't rely on dynamic
+ * registration any more, because ioctls need to know
+ * it. */
+#define MAJOR_NUM 245
+#define NUM_CHANNELS 4
+#define MESSAGE_BUFFER_LENGTH 128
+#define SUCCESS 0
+#define FAIL -1
+
+/* Set the message of the device driver */
+#define IOCTL_SET_ENC _IOW(MAJOR_NUM, 0, unsigned long)
+
+#define DEVICE_RANGE_NAME "message_slot"
+#define DEVICE_FILE_NAME "message_slot_file"
+
+
+//// FROM CHARDEV.H: ^^^^^^^^^^^^^^^
+
+
+
 
 #endif /* MESSAGE_SLOT_H_ */
